202be61d94a94227d77ff69aa25b8c41b77c21a5
chandan123-sik <sikarwarnihal191@gmail.com>
2025-04-06 08:57:56 +0530

    Add files via upload

---
+++ Timer/first.js
@@ -0,0 +1,74 @@
+// function timing(){
+// const timer = document.getElementById('root');
+// const now = new Date();
+// const time = now.toLocaleTimeString();
+// timer.innerHTML = time;
+// }
+// setInterval(timing,1000);
+
+// const timer = document.getElementById('root');
+// timer.style.fontSize = "100px";
+// timer.style.height = "100vh";
+// timer.style.display = "flex";
+// timer.style.justifyContent = "center";
+// timer.style.alignItems = "center";
+
+
+// Accessing Elements :-
+
+// css selector:
+// const id = document.querySelector('#first');
+// id.innerHTML = "chandan sikarwar";
+
+// const id2 = document.querySelector('.header2');
+// console.log(id2);
+// id2.style.backgroundColor = "pink";
+
+// styling change
+// for(i=0;i<obj.length;i++)
+//     obj[i].style.color = "red";
+
+// iterate
+// for(i=0;i<obj.length;i++)
+//     console.log(obj[i]);
+
+// for(let val of obj)
+// console.log(val);
+
+// obj.foreach((hel)=>{
+//  console.log(hel);
+// })
+
+// tag name
+
+// let team = document.getElementsByTagName('li');
+// console.log(team);
+
+// iterate:
+// for(i=0;i<team.length;i++)
+//     team[i].style.color = "yellow";
+
+// Array.from(team).forEach((hel)=>{
+//     console.log(hel);
+//    })
+
+//    for(let val of team)
+//     console.log(val)
+
+// accessing using relationship:
+// const list = document.querySelector('li');
+// console.log(list.parentNode);
+
+// const par = document.querySelector('ul');
+// console.log(par);
+// console.log(par.childNodes);
+// console.log(par.children);
+
+const par = document.querySelector('ul');
+console.log(par);
+// console.log(par.firstChild);
+// console.log(par.lastChild);
+
+// console.log(par.firstElementChild);
+// console.log(par.lastElementChild);
+
